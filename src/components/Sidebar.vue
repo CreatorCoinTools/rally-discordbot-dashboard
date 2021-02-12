7216bdce5645c8f167140a6c29f31e9604ecb0c2
hattyot <hattyot@gmail.com>
2021-02-12 11:33:16 +0200

    added better customize image and hide customize when user hasn't selected a guild

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -114,17 +114,13 @@
               <span>{{ $t("sidebar.settings") }}</span>
             </router-link>
             <router-link
+                v-if="currentGuild !== null"
                 tag="div"
                 :to="{ path: 'customize' }"
                 class="w-full flex items-center h-10 pl-4 hover:text-red-400 rounded-lg cursor-pointer"
             >
-              <svg class="h-6 w-6 fill-current mr-2" viewBox="0 0 20 20">
-                <path
-                    d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"
-                />
-                <path
-                    d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"
-                />
+              <svg style="fill:#9e9e9e" class="h-6 w-6 mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M8.071 21.586l-7.071 1.414 1.414-7.071 14.929-14.929 5.657 5.657-14.929 14.929zm-.493-.921l-4.243-4.243-1.06 5.303 5.303-1.06zm9.765-18.251l-13.3 13.301 4.242 4.242 13.301-13.3-4.243-4.243z"/>
               </svg>
               <span>{{ $t("sidebar.customize") }}</span>
             </router-link>
